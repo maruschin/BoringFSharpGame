d3cbbbc8a8ecfa1e1ff4cb7f1b455f3652fcf2f3
Eugene Maruschin (Evgenii Maruschenko) <maruschin@gmail.com>
2018-12-22 22:26:29 +1000

    Добился отображения ассетов
    Прошел 4 часть статьи по MonoGame и F# https://bruinbrown.wordpress.com/2013/10/06/making-a-platformer-in-f-with-monogame/
    
    Столкнулся с проблемой загрузки ассетов, сейчас нельзя напрямую загрузить png, нужно преобразовывать файлы в xnb. Грузить png можно, но нужно изучать другой механизм.

--- src/Core.fs
+++ src/Core.fs
@@ -4,11 +4,30 @@ open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
 
+open Actor
+
 type BoringGame () as this =
     inherit Game()
 
+    do this.Content.RootDirectory <- "Content"
+
     let mutable Graphics = new GraphicsDeviceManager(this)
     let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    let CreateActor' = CreateActor this.Content
+
+    let mutable WorldObjects = lazy ([("player", Player(Nothing),
+                                       Vector2(10.f,28.f), Vector2(32.f,32.f), false);
+                                      ("obstacle", Obstacle,
+                                       Vector2(10.f,60.f), Vector2(32.f,32.f), true);
+                                      ("", Obstacle,
+                                       Vector2(42.f, 60.f), Vector2(32.f,32.f), true);] 
+                                     |> List.map CreateActor')
+
+    let DrawActor (sb:SpriteBatch) actor =
+        if actor.Texture.IsSome then
+            do sb.Draw(actor.Texture.Value, actor.Position, Color.White)
+        ()
 
     override this.Initialize() =
         do spriteBatch <- new SpriteBatch(this.GraphicsDevice)
@@ -17,7 +36,7 @@ type BoringGame () as this =
         ()
 
     override this.LoadContent() =
-        // TODO: use this.Content to load your game content here
+        do WorldObjects.Force () |> ignore
         ()
 
     override this.Update (gameTime) =
@@ -26,6 +45,9 @@ type BoringGame () as this =
 
     override this.Draw (gameTime) =
         do this.GraphicsDevice.Clear Color.CornflowerBlue
-        // TODO: Add your drawing code here
+        let DrawActor' = DrawActor spriteBatch
+        do spriteBatch.Begin ()
+        WorldObjects.Value |> List.iter DrawActor'
+        do spriteBatch.End ()
         ()
 
