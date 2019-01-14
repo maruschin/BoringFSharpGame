60a3e8c8c135414ac4b5ae66738d344e61ace5d7
Maruschin Eugene <maruschin@gmail.com>
2019-01-14 13:55:33 +1000

    Перерисовал модель акторов, все должно поломаться, во славу хаоса

--- src/Actor.Domain.fs
+++ src/Actor.Domain.fs
@@ -5,58 +5,69 @@ open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Content
 open Animation
 
-type BodyType =
-    | Static
-    | Dynamic of Vector2
+type Direction =
+	| Forward
+	| Backward
 
-type PlayerState =
-    | Nothing
-    | Jumping
+type Action =
+	| Idle
+	| Moving of Direction
+	| jumping
+	| JumpingTo of Direction
+
+type Animation =
+	| Animated of Animation
+	| Static of Texture2D
 
 type ActorType =
-    | Player of PlayerState
-    | Obstacle
-    | BackGround
+	| Player of Action
+	| Enemy of Action
+	| Obstacle
+	| BackGround
 
-type AnimationType =
-    | Animated of Animation
-    | NotAnimated of Texture2D
+type BodyType =
+	| Static
+	| Dynamic
 
-type WorldActor =
-    {
-        ActorType : ActorType;
-        Position : Vector2;
-        Size : Vector2;
-        Animation : AnimationType;
-        BodyType : BodyType
-    }
-    member this.CurrentBounds =
-        Rectangle((int this.Position.X),
-                  (int this.Position.Y),
-                  (int this.Size.X),
-                  (int this.Size.Y))
-    
-    member this.DesiredBounds =
-        let desiredPos =
-            match this.BodyType with
-            | Dynamic(s) -> this.Position + s
-            | Static -> this.Position
-        Rectangle((int desiredPos.X),
-                  (int desiredPos.Y),
-                  (int this.Size.X),
-                  (int this.Size.Y))
+type Size = Size of Vector2
 
-let CreateActor (content:ContentManager) (textureName, actorType, position, size, bodyType) =
-    let tex = content.Load textureName
-    let animation =
-        match actorType with
-        | Player(s) -> Animated(CreateAnimation tex 100)
-        | Obstacle -> NotAnimated(tex)
-        | BackGround -> NotAnimated(tex)
-    {
-        ActorType = actorType;
-        Position = position;
-        Size = size;
-        Animation = animation;
-        BodyType = bodyType;
-    }
+type Position = Position of Vector2
+
+let getBounds (position:Position) (size:Size) =
+	Rectangle((int position.X),
+	          (int position.Y),
+	          (int size.X),
+	          (int size.Y))
+
+type Actor =
+	{
+		Type : ActorType;
+		Position : Position;
+		Size : Size;
+		Animation : Animation;
+	}
+
+	member this.CurrentBounds =
+		getBounds this.Position this.Size
+
+	member this.DesiredBounds =
+		match this.BodyType with
+		| Static -> getBound this.Position this.Size
+		| Dynamic(s) -> getBound this.Position + s this.Size
+
+let Create Actor (content:ContentManager) (actorType:ActorType) (bodyType:BodyType) (size:Size) (position:Position) (textureName) =
+	let tex = content.Load textureName
+	let animation =
+		match actorType with
+		| Player(s)
+		| Enemy(s) -> Animated(CreateAnimation tex 100)
+		| Obstacle
+		| Background -> NotAnimated(tex)
+
+	{
+		ActorType = actorType;
+		Position = position;
+		Size = size;
+		Animation = animation;
+		BodyType = bodyType;
+	}
